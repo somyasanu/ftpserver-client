5183c141ca0594e24b00c24b44caad210247b919
somyasanu <68684186+somyasanu@users.noreply.github.com>
2020-08-05 20:58:46 +0530

    Add files via upload

---
+++ UDPclient.java
@@ -0,0 +1,23 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPclient{
+  public static void main(String[] args) throws Exception
+    {
+           
+           DatagramSocket ds=new DatagramSocket();
+           int i =5;
+           byte[] b=(i+"").getBytes();
+           InetAddress ia= InetAddress.getLocalHost();
+           DatagramPacket dp= new DatagramPacket(b,b.length,ia,9999);
+           ds.send(dp);
+           
+           byte[] b1=new byte[1024];
+           DatagramPacket dp1= new DatagramPacket(b1,b1.length);
+           ds.receive(dp1);
+      
+           String str= new String(dp1.getData());
+           System.out.println("received package is "+str);
+    }
+}
